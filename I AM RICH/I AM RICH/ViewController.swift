191fb353d736563d4195974d39819f8d7dc83f2d
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-10-23 19:30:42 +0530

    inital

---
+++ I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Sarthak Agrawal on 23/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I AM RICH/I AM RICHUITests/I_AM_RICHUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  I_AM_RICHUITestsLaunchTests.swift
+//  I AM RICHUITests
+//
+//  Created by Sarthak Agrawal on 23/10/21.
+//
+
+import XCTest
+
+class I_AM_RICHUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
